152d7ea378d2e890aaff34b4244c4d9247ac7467
Krunoslav Zaher <krunoslav.zaher@gmail.com>
2015-09-23 11:58:50 +0200

    Improves current thread scheduler and solves variable confusing behavior on dealloc.

--- RxSwift/Schedulers/CurrentThreadScheduler.swift
+++ RxSwift/Schedulers/CurrentThreadScheduler.swift
@@ -46,7 +46,15 @@ public class CurrentThreadScheduler : ImmediateSchedulerType {
             return NSThread.currentThread().threadDictionary[CurrentThreadSchedulerKeyInstance] as? ScheduleQueue
         }
         set {
-            NSThread.currentThread().threadDictionary[CurrentThreadSchedulerKeyInstance] = newValue
+            print("Before removing object")
+            let threadDictionary = NSThread.currentThread().threadDictionary
+            if let newValue = newValue {
+                threadDictionary[CurrentThreadSchedulerKeyInstance] = newValue
+            }
+            else {
+                threadDictionary.removeObjectForKey(CurrentThreadSchedulerKeyInstance)
+            }
+            print("After removing object")
         }
     }
     
